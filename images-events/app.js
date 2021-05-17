d099f38c3dc02669243a324a1aac67adafdd5f23
Stephen Castaneda <scastaneda@pivottechschool.com>
2021-05-17 16:23:59 -0500

    added starter files

---
+++ images-events/app.js
@@ -0,0 +1,15 @@
+//TDD 
+// Make objects for each image - name, image, clicks - seen? (check)
+// constructor function ^^ (check)
+// create count variable (check)
+// counting guesses ONLY allowing a certain number and then you can't guess any more
+// render two images - must be two different images
+// - renders
+// - pick goats
+// you can click - listener
+// when you click it counts AND renders new images
+// stretch goal - make sure images don't repeat each round 
+// wipe out data of results to start over - render the results
+// Get global variables
+
+// Global Variables
